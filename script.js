2feaf98392c1cef506e9d76d630c0fa7c366ec24
shailesh74250 <smourya7425@gmail.com>
2020-08-20 15:06:45 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('btnBar').addEventListener('click', myFunction);
+
+function myFunction() {
+  document.getElementById('btnBar').classList.toggle('btn-left');
+  document.getElementById('mobo').classList.toggle('mobo-menu');
+}
